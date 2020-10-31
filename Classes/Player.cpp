cfdc8c7f0744a3a402a4317209666df85a028055
0dj3 <xaryshal@gmail.com>
2020-10-31 13:46:19 +0900

    Added edgebox

--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -12,10 +12,12 @@ Player::Player()
 
 Unit* Player::create(cocos2d::Scene* scene)
 {
+    Sprite sprite;
     Player* newPlayer = new Player();
-    if (newPlayer->sprite->initWithFile("CloseNormal.png")) {
-        auto body = PhysicsBody::createCircle(newPlayer->sprite->getContentSize().width / 2);
+    if (newPlayer->sprite->initWithFile("test_hero.png")) {
+        auto body = PhysicsBody::createBox(Size(newPlayer->sprite->getContentSize().width / 2, newPlayer->sprite->getContentSize().height / 2),PhysicsMaterial());
         body->setContactTestBitmask(true);
+        newPlayer->setScale(3.0);
         newPlayer->setPhysicsBody(body);
         newPlayer->scene = scene;
         newPlayer->autorelease();
